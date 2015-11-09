e880e9ff4e28d36173bbd81b916ca8b7f9e3f7e2
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-08 23:11:43 -0800

    if statements done

--- javascripting/if-statement.js
+++ javascripting/if-statement.js
@@ -1,1 +1,7 @@
 var fruit = 'orange';
+
+if ( fruit.length > 5 ) {
+  console.log("The fruit name has more than five characters.");
+} else {
+  console.log("THe fruit name has five characters or less.");
+} 
