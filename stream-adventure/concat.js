85cc07d5ae51cb9d34fc9fc04ebc2d1f32995f20
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-12 08:17:09 -0800

    added reference solution

--- stream-adventure/concat.js
+++ stream-adventure/concat.js
@@ -5,3 +5,12 @@ process.stdin.pipe(
            var s = body.toString().split('').reverse().join('');
            console.log(s);
          }));
+
+// Here's the reference solution:
+//
+// var concat = require('concat-stream');
+//
+// process.stdin.pipe(concat(function (src) {
+//     var s = src.toString().split('').reverse().join('');
+//     console.log(s);
+// }));
