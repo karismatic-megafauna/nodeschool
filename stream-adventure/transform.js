d00f53269eeaaa0faffa0b1d04db3140f9238956
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-11-14 17:57:03 -0800

    reference solution for transforms

--- stream-adventure/transform.js
+++ stream-adventure/transform.js
@@ -8,3 +8,12 @@ process.stdin.pipe(
     done();
   })
 ).pipe(process.stdout);
+
+// Here is the reference solution:
+// they did it a little better by pulling out the through2 def
+var through = require('through2');
+var tr = through(function (buf, _, next) {
+  this.push(buf.toString().toUpperCase());
+  next();
+});
+process.stdin.pipe(tr).pipe(process.stdout);
